301870c13fe9fc356ac4aae4402c1699fcf50ddf
teodosi.kachakov <teo.kachakov@gmail.com>
2020-07-09 17:07:50 +0300

    this commit is from pycharm, exercises 10 list overlap comprehensions

--- 09.guessing_game_one.py
+++ 09.guessing_game_one.py
@@ -1,14 +1,20 @@
 import random
 print("Guess a random number between 1 and 9 inclusive")
 rand_number = random.randint(1, 9)
+num_guesses = 0
 while True:
-    guess = int(input("Guess a number: "))
-    if guess == rand_number:
-        print("That's correct!")
+    guess = input("Guess a number: ")
+    if guess == 'exit':
+        print(f'You guessed {num_guesses} times!')
         break
-    elif guess > rand_number:
+    if int(guess) == rand_number:
+        print("That's correct!")
+        num_guesses += 1
+    elif int(guess) > rand_number:
         print("Go lower!")
+        num_guesses += 1
     else:
         print("Go higher!")
+        num_guesses += 1
 
 #see extra stuff for this
